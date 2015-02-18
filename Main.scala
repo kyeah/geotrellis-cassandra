4e5875ef66d21594539a48b70cbb9b82eab75dbe
Kevin Yeh <kevinyeah@utexas.edu>
2015-02-18 16:13:38 -0600

    Fix dependency versions and script

--- Main.scala
+++ Main.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.{SparkConf, SparkContext}
 import com.datastax.spark.connector._
+import com.datastax.spark.connector.streaming._
 
 object Main {
   def main(args: Array[String]) {
@@ -7,17 +8,17 @@ object Main {
       .set("spark.cassandra.connection.host", "127.0.0.1")
     
     conf.setJars(Seq(
-      "lib/spark-cassandra-connector-assembly-1.2.0-SNAPSHOT.jar",
-      "target/scala-2.10/geotrellis-cassandra-assembly-0.1.jar"))
+    "lib/spark-cassandra-connector-assembly-1.2.0-SNAPSHOT.jar",
+    "target/scala-2.10/geotrellis-cassandra-assembly-0.1.jar"))
 
     val sc = new SparkContext("spark://kyeh-yoga:7077", "test", conf)
     val rdd = sc.cassandraTable("test", "kv")
 
     println(rdd.count)
-    println(rdd.first) // Not serializable error
-    //println(rdd.map(_.getInt("value")).sum)  // Anon function missing error
+    //println(rdd.first) // Not serializable error
+    //println(rdd.map(_.getInt("value")).count)  // Anon function missing error on .count
     
-    //val collection = sc.parallelize(Seq(("key3", 3), ("key4", 4)))
-    //collection.saveToCassandra("test", "kv", SomeColumns("key", "value"))  // Classdefnotfound ..spark.streaming.Duration
+    val collection = sc.parallelize(Seq(("key3", 3), ("key4", 4)))
+    collection.saveToCassandra("test", "kv", SomeColumns("key", "value"))
   }
 }
--- build.sbt
+++ build.sbt
@@ -3,5 +3,13 @@ version := "0.1"
 scalaVersion := "2.10.4"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "1.2.1",
-  "com.datastax.spark" %% "spark-cassandra-connector" % "1.1.0"
+  "org.apache.spark" %% "spark-streaming" % "1.2.1",
+  "com.datastax.spark" %% "spark-cassandra-connector" % "1.2.0-alpha2"
 )
+
+mergeStrategy in assembly <<= (mergeStrategy in assembly) {
+  old => {
+    case PathList("META-INF", "MANIFEST.MF") => MergeStrategy.discard
+    case x => MergeStrategy.last
+  }
+}
